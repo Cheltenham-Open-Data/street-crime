b47da628dfed36dbceab0fc3dada8ae6ccfa620f
MatBenfield <MatBenfield@users.noreply.github.com>
2020-11-12 19:28:45 +0000

    updates, refactor, move download to build
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- python/downloader.py
+++ python/downloader.py
@@ -4,12 +4,6 @@ import json
 import pathlib
 import os
 
-lat1 = "51.9042"
-lon1 = "-2.10141"
-append = f"lat={lat1}&lng={lon1}"
-endpoint1 = f"https://data.police.uk/api/crimes-street/stops-street?{append}"
-endpoint2 = f"https://data.police.uk/api/crimes-street/all-crime?{append}"
-
 def get_data(endpoint):
     print(endpoint)
     response = get(endpoint, timeout=20)
@@ -18,11 +12,17 @@ def get_data(endpoint):
         print(f"Request failed: { response.text }")
     return response.json()
 
-# output
 if __name__ == "__main__":
     root = pathlib.Path(__file__).parent.parent.resolve()
+
+    with open( root / "data/AA3_stops_street.json", 'r+') as filehandle:
+        data = json.load(filehandle)
+        new_data = get_data("https://data.police.uk/api/crimes-street/stops-street?lat=51.9042&lng=-2.10141")
+        filehandle.seek(0)
+        json.dump(new_data, filehandle, indent=4)
+
     with open( root / "data/AA3_all_crime.json", 'r+') as filehandle:
         data = json.load(filehandle)
-        new_data = get_data(endpoint1)
+        new_data = get_data("https://data.police.uk/api/crimes-street/all-crime?lat=51.9042&lng=-2.10141")
         filehandle.seek(0)
         json.dump(new_data, filehandle, indent=4)
